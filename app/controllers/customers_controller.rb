3f093532a55b0e01d730ceda34632712dc726386
empireofryan <rjohnson2001@gmail.com>
2016-04-04 02:02:12 -0400

    Done.

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,15 +1,11 @@
 class CustomersController < ApplicationController
-  require 'csv'
-
+ 
   def index
     @customers = Customer.all
   end
-
+ 
   def upload
-    CSV.foreach(params[:leads].path, headers: true) do |lead|
-      Customer.create(email: lead[0], first_name: lead[1], last_name: lead[2])
-    end
+    LeadsWorker.perform_async(params[:leads].path)
     redirect_to customers_path
   end
-
 end
