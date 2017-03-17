3ef40cf3b00777a105c1647fc4b31bbafe477810
smithwebtek <brad@smithwebtek.com>
2017-03-17 01:13:56 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :customers, only: [:index]
   post 'customers/upload', to: 'customers#upload'
+  get '/clear', to: 'customers#clear_leads'
+  root 'customers#index'
 end
