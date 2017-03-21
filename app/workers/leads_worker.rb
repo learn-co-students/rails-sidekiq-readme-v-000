c98357a01bc299bdf628a98de1433bae3d7d261b
Brad Watson <brad.watson.orlando@gmail.com>
2017-03-21 14:55:02 -0400

    done

---
+++ app/workers/leads_worker.rb
@@ -0,0 +1,11 @@
+class LeadsWorker
+  require 'csv'
+  include Sidekiq::Worker
+
+  def perform(leads_file)
+    CSV.foreach(leads_file, headers: true) do |lead|
+      Customer.create(email: lead[0], first_name: lead[1], last_name: lead[2])
+    end
+  end
+  
+end
