e9b1925df0621d78e14ca5aae9a38265823ba232
James Fernandez <jffernan@comcast.net>
2017-08-28 18:30:17 -0700

    Update customers_controller.rb

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,7 +1,10 @@
 class CustomersController < ApplicationController
-  require 'csv'
-
   def index
     @customers = Customer.all
+  end
+ 
+  def upload
+    LeadsWorker.perform_async(params[:leads].path)
+    redirect_to customers_path
   end
 end
