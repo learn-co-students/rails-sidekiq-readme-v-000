0207b36d429f2f996b78498ce56a3e1d95afb31a
James Fernandez <jffernan@comcast.net>
2017-08-28 18:29:43 -0700

    Update Leads_worker.rb

--- app/workers/Leads_worker.rb
+++ app/workers/Leads_worker.rb
@@ -1,15 +1,10 @@
 class LeadsWorker
+  require 'csv'
   include Sidekiq::Worker
  
   def perform(leads_file)
- 
-  end
-  
-  def upload
-    CSV.foreach(params[:leads].path, headers: true) do |lead|
+    CSV.foreach(leads_file, headers: true) do |lead|
       Customer.create(email: lead[0], first_name: lead[1], last_name: lead[2])
     end
-    redirect_to customers_path
   end
-
 end
