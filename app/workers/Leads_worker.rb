93043db949433b7b6876d4146d6c95d3b726ecd8
James Fernandez <jffernan@comcast.net>
2017-08-28 18:28:54 -0700

    Update Leads_worker.rb

--- app/workers/Leads_worker.rb
+++ app/workers/Leads_worker.rb
@@ -4,4 +4,12 @@ class LeadsWorker
   def perform(leads_file)
  
   end
+  
+  def upload
+    CSV.foreach(params[:leads].path, headers: true) do |lead|
+      Customer.create(email: lead[0], first_name: lead[1], last_name: lead[2])
+    end
+    redirect_to customers_path
+  end
+
 end
