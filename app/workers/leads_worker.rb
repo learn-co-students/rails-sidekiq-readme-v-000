644128615bf940e6e19fbb747f2ba09a99ed03e5
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-05-01 17:40:50 -0400

    Done.

---
+++ app/workers/leads_worker.rb
@@ -0,0 +1,10 @@
+class LeadsWorker
+  include Sidekiq::Worker
+  require 'csv'
+
+  def perform(leads_file)
+    CSV.foreach(leads_file, headers: true) do |lead|
+      Customer.create(email: lead[0], first_name: lead[1], last_name: lead[2])
+    end
+  end
+end
