dca1ba9540e7c994d0e5e0f4a3123a7e470846af
Emily Jirles <emily.jirles@gmail.com>
2018-07-19 16:03:21 +0000

    Done.

--- app/workers/leads_worker.rb
+++ app/workers/leads_worker.rb
@@ -6,5 +6,5 @@ class LeadsWorker
         CSV.foreach(leads_file, headers: true) do |lead|
             Customer.create(email: lead[0], first_name: lead[1], last_name: lead[2])
         end
-    end 
-end 
+    end
+end
