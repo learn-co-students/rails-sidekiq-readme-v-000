4c43679a76ff7a8a7be5054f54049acf6aa4f908
Jirles <emily.jirles@gmail.com>
2018-07-19 11:23:40 -0400

    revert perform to instance method

--- app/workers/leads_worker.rb
+++ app/workers/leads_worker.rb
@@ -2,7 +2,7 @@ class LeadsWorker
     require 'csv'
     include Sidekiq::Worker
 
-    def self.perform(leads_file)
+    def perform(leads_file)
         CSV.foreach(leads_file, headers: true) do |lead|
             Customer.create(email: lead[0], first_name: lead[1], last_name: lead[2])
         end
