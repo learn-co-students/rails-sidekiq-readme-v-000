d9926ba57cc57ffc2ac5e72d8ad26ec328842936
Dillorom <dabdulloeva@gmail.com>
2018-09-14 11:09:13 -0700

    Done.

--- Gemfile
+++ Gemfile
@@ -45,5 +45,7 @@ group :development do
   gem 'spring'
 
   gem 'sidekiq'
+  gem 'thin'
+  gem 'pry'
 end
 
--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,12 +1,11 @@
 class CustomersController < ApplicationController
-
+ 
   def index
     @customers = Customer.all
   end
-
+ 
   def upload
-    LeadWorker.perform_async(params[:leads].path)
+    LeadsWorker.perform_async(params[:leads].path)
     redirect_to customers_path
   end
-
 end
