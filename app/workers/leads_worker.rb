db7569ee89e2c75eb1ac4d04b5bc606f04de0a69
Saad Ahmed <dropheaven@live.com>
2019-03-21 07:24:27 -0400

    Done.

---
+++ app/workers/leads_worker.rb
@@ -0,0 +1,10 @@
+class LeadsWorker
+  require 'csv'
+  include SideKiq::Worker
+
+  def perform(leads_file)
+    CSV.foreach(params[:leads].path, headers: true) do |lead|
+      Customer.create(email: lead[0], first_name: lead[1], last_name: lead[2])
+    end
+  end
+end
