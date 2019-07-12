3ab1c2b5f219bc2d97560508c142e94166b0f5d7
sjwakean <swakeman@ca.rr.com>
2019-07-12 14:43:08 -0700

    Done.

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -5,11 +5,18 @@ class CustomersController < ApplicationController
     @customers = Customer.all
   end
 
+# All you have to do is take the long-running code out of one place def upload, 
+# and put it inside of perform:
   def upload
-    CSV.foreach(params[:leads].path, headers: true) do |lead|
+    LeadsWorker.perform_async(params[:leads].path)
+      redirect_to customers_path
+  end
+
+
+  def perform(leads_file)
+    CSV.foreach(leads_file, headers: true) do |lead|
       Customer.create(email: lead[0], first_name: lead[1], last_name: lead[2])
     end
-    redirect_to customers_path
   end
 
 end
